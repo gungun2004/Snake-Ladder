94553b794f00770b1dfd05b0cace9d70dfbc2a7d
Gunjan <gunjanaggarwal278@gmail.com>
2025-07-26 19:05:46 +0530

    .

--- Assets/Scripts/PathPoint.cs
+++ Assets/Scripts/PathPoint.cs
@@ -4,20 +4,18 @@ using System.Collections.Generic;
 public class PathPoint : MonoBehaviour
 {
     public PathObjectPoint pathObjectPoint;
-   public List<PlayerPiece> playerPieceList = new List<PlayerPiece>();
+    public  List<PlayerPiece> playerPieceList = new List<PlayerPiece>();
+
     void Start()
     {
         pathObjectPoint = GetComponentInParent<PathObjectPoint>();
     }
 
-    // Update is called once per frame
-     public void Awake()
-    {
-        
-    }
+   
     public void AddPlayerPiece(PlayerPiece playerPiece_)
     {
         playerPieceList.Add(playerPiece_);
+        RescaleandRepositioningAllPlayerPiece();
     }
     public void RemovePlayerPiece(PlayerPiece  playerPiece_)
     {
@@ -30,33 +28,38 @@ public class PathPoint : MonoBehaviour
 
     public void RescaleandRepositioningAllPlayerPiece()
     {
-        int plsCont = playerPieceList.Count;
-        bool isOdd = (plsCont/2)==0?false:true;
+        int plsCount = playerPieceList.Count;
+        if (plsCount == 0) return;
 
-        int extent = plsCont / 2;
-        int counter= 0;
-        int SpriteLayer = 0;
+        bool isOdd = (plsCount / 2) == 0 ?false :true;
 
-        if(isOdd)
+        int extent = plsCount / 2;
+        int counter = 0;
+        int spriteLayer = 0;
+        if (isOdd)
         {
-            for(int i = -extent ; i<=extent;i++)
+            for (int i = -extent; i <= extent; i++)
             {
-                playerPieceList[counter].transform.localScale = new Vector3(pathObjectPoint.scales[plsCont-1],pathObjectPoint.scales[plsCont-1],1f);
-                playerPieceList[counter].transform.position = new Vector3(transform.position.x+(i*pathObjectPoint.positionDifference[plsCont-1]),transform.position.y,0f);
+                playerPieceList[counter].transform.localScale = new Vector3(pathObjectPoint.scales[plsCount - 1], pathObjectPoint.scales[plsCount - 1], 1f);
+
+                playerPieceList[counter].transform.position = new Vector3(transform.position.x + (i * pathObjectPoint.positionDifference[plsCount - 1]), transform.position.y, 0f);
             }
         }
         else
         {
-           for(int i = -extent ; i < extent;i++)
-            {
-                playerPieceList[counter].transform.localScale = new Vector3(pathObjectPoint.scales[plsCont-1],pathObjectPoint.scales[plsCont-1],1f);
-                playerPieceList[counter].transform.position = new Vector3(transform.position.x+(i*pathObjectPoint.positionDifference[plsCont-1]),transform.position.y,0f);
+            for (int i = -extent; i < extent; i++) {
+
+                playerPieceList[counter].transform.localScale = new Vector3(pathObjectPoint.scales[plsCount - 1], pathObjectPoint.scales[plsCount - 1], 1f);
+
+                playerPieceList[counter].transform.position = new Vector3(transform.position.x + (i * pathObjectPoint.positionDifference[plsCount - 1]), transform.position.y, 0f);
             }
+        
         }
-        for(int i=0; i<playerPieceList.Count;i++)
+
+        for (int i = 0; i < playerPieceList.Count; i++)
         {
-            playerPieceList[i].GetComponentInChildren<SpriteRenderer>().sortingOrder = SpriteLayer;
-            SpriteLayer ++;
+            playerPieceList[i].GetComponentInChildren<SpriteRenderer>().sortingOrder = spriteLayer++;
         }
     }
+
 }
